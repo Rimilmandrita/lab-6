63c81d8b84049bd60d171569ddb48cfa180e9423
Rimilmandrita <31147874+Rimilmandrita@users.noreply.github.com>
2017-10-27 23:56:41 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -2,23 +2,26 @@
 using namespace std;
 
 int main() {
-int a;float f;double d;char c;
-	cout<<"enter integer,float,double and character values\n";
+int a;float f;double d;char c,bool b;
+	cout<<"enter integer,float,double,bool and character values\n";
 	cin>>a>>f>>d>>c;
-	int *p;float *p2;double *p3; char *p4;
+	int *p;float *p2;double *p3; char *p4;bool *p5;
 	p=&a;
 	p2=&f;
 	p3=&d;
 	p4=&c;
+	p5=&b;
 	cout<<"the size of interger variable is "<<sizeof(a)<<"\n";
 	cout<<"the size of float variable is "<<sizeof(f)<<"\n";
 	cout<<"the size of double variable is "<<sizeof(d)<<"\n";
-	cout<<"the size of character variable is "<<sizeof(c);
+	cout<<"the size of character variable is "<<sizeof(c)<<"\n";
+	cout<<"the size of boolean variable is "<<sizeof(b);
 	
 	cout<<"\nthe size of interger type pointer is "<<sizeof(p)<<"\n";
 	cout<<"the size of float type pointer is "<<sizeof(p2)<<"\n";
 	cout<<"the size of double type pointer is "<<sizeof(p3)<<"\n";
-	cout<<"the size of character type pointer is "<<sizeof(p4);
+	cout<<"the size of character type pointer is "<<sizeof(p4)<<"\n";
+	cout<<"the size of boolean type pointer is "<<sizeof(p5);
 	
 	return 0;
 }
