a96203ea4fe523a6f42ec1c185c877a1026d4663
Rimilmandrita <31147874+Rimilmandrita@users.noreply.github.com>
2017-11-20 01:07:25 +0530

    Update Q4.new.cpp

--- Q4.new.cpp
+++ Q4.new.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main() {
+string name;
+char *p;
+cout<<"Enter your name\n";
+getline(cin,name);
+int n=name.size();
+p=&(name[0]);
+
+for(int i=0;i<n;i++)
+ {
+  cout<<*(p+i);
+}
+ cout<<endl;
+for (int i=0;i<n;i++)
+
+ { 
+   cout<<name[i];
+ }
+return 0;
+}
+
 
